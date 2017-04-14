c5d490b552ee9dea836862b8c955a21a44d0b2be
szilard <szilard@users.noreply.github.com>
2017-04-14 04:05:09 -0700

    wk-06-ML LR glmnet

---
+++ wk-06-ML/1-LR/1-glmnet.R
@@ -0,0 +1,38 @@
+
+library(readr)
+library(glmnet)
+library(ROCR)
+
+
+d <- read_csv("wk-06-ML/data/airline100K.csv")
+
+set.seed(123)
+N <- nrow(d)
+idx <- sample(1:N, 0.6*N)
+d_train <- d[idx,]
+d_test <- d[-idx,]
+
+
+X <- Matrix::sparse.model.matrix(dep_delayed_15min ~ . - 1, data = d)
+X_train <- X[idx,]
+X_test <- X[-idx,]
+
+
+system.time({
+  md <- glmnet( X_train, d_train$dep_delayed_15min, family = "binomial", lambda = 0)
+})
+## stats:glm very slow!!!
+
+
+system.time({
+  phat <- predict(md, newx = X_test, type = "response")
+})
+
+rocr_pred <- prediction(phat, d_test$dep_delayed_15min)
+performance(rocr_pred, "auc")@y.values[[1]]
+
+
+
+
+
+
