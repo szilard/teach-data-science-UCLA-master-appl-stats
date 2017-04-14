1de01b8f205984e292ff351b3d2ffb2c53e64d71
szilard <szilard@users.noreply.github.com>
2017-04-14 03:50:00 -0700

    wk-06-ML data

--- wk-06-ML/0-getdata/getdata.R
+++ wk-06-ML/0-getdata/getdata.R
@@ -3,6 +3,6 @@
 # see https://github.com/szilard/benchm-ml
 
 data_url <- "https://s3.amazonaws.com/benchm-ml--main/train-0.1m.csv"
-data_localfile <- "wk-06-ML/data/data.csv"
+data_localfile <- "wk-06-ML/data/airline100K.csv"
 download.file(data_url, data_localfile)
 
