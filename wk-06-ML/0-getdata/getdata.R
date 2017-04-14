336d5217d08b4c638072b85e8f8150b8adff4b0f
szilard <szilard@users.noreply.github.com>
2017-04-14 03:43:49 -0700

    wk-06-ML data

---
+++ wk-06-ML/0-getdata/getdata.R
@@ -0,0 +1,8 @@
+
+# 100K rows from the airline data set
+# see https://github.com/szilard/benchm-ml
+
+data_url <- "https://s3.amazonaws.com/benchm-ml--main/train-0.1m.csv"
+data_localfile <- "wk-06-ML/data/data.csv"
+download.file(data_url, data_localfile)
+
